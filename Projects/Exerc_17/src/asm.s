98f70cdf1e474f78225636d5131b56a8623df42b
darthguima <42544945+darthguima@users.noreply.github.com>
2021-07-23 11:47:10 -0300

    Exerc_17 atualizado

--- Projects/Exerc_17/src/asm.s
+++ Projects/Exerc_17/src/asm.s
@@ -28,21 +28,111 @@ __iar_program_start
         
         
 main    
-        BL config_N //CONFIGUTA AS SAÍDA DOS LEDS D1 E D2, R2 COMO BASE
-        BL config_F //CONFIGUTA AS SAÍDA DOS LEDS D3 E D4, R3 COMO BASE
+        BL config_N //CONFIGURA AS SAÍDA DOS LEDS D1 E D2, R2 COMO BASE
+        BL config_F //CONFIGURA AS SAÍDA DOS LEDS D3 E D4, R3 COMO BASE
 
  	LDR R0, = GPIO_PORTN_DATA_R
         LDR R4, = GPIO_PORTF_DATA_R
         
-loop	STR R1, [R0, R2, LSL #2] ; aciona LED com estado atual
+loop2   MOV R6, #0
+        MOV R7, #00000000b
+        MOV R8, #00000000b
+loop	STR R7, [R0, R2, LSL #2] ; aciona LED com estado atual
+        STR R8, [R4, R3, LSL #2]
         MOVT R5, #0x000F ; constante de atraso 
 delay   CBZ R5, theend ; 1 clock
         SUB R5, R5, #1 ; 1 clock
         B delay ; 3 clocks
-theend  EOR R1, R1, R2 ; troca o estado
-        B loop
+theend  ADD R6, #1
+        MOV R9, R6 ;UTILIZAÇÃO DE R9 PARA ENCURTAR UM POUCO A QUANTIDADE DE CÓDIGO, JÁ QUE A MUDANÇA BIT A BIT PODE SER FEITA COM REPETIÇÕES DOS 
+        B comps    ; DE 1, 2 E 4.
         
 
+comps   
+        CMP R6, #16
+        BEQ loop2
+        
+        
+        
+num1    CMP R9, #1
+        ITT EQ
+        ORREQ R7, R7, #0x02
+          BEQ loop
+          
+num2    CMP R9, #2
+        ITTT EQ
+          BICEQ R7, R7, #0x02
+          ORREQ R7, R7, #0x01
+          BEQ loop
+          
+num3    CMP R9, #3
+        ITT EQ
+          MOVEQ R9, #1
+          BEQ num1
+          
+num4    CMP R9, #4
+        ITTT EQ
+          ORREQ R8, R8, #0x10
+          BICEQ R7, R7, #0x03
+          BEQ loop
+          
+        CMP R9, #5
+        ITT EQ
+          MOVEQ R9, #1
+          BEQ num1
+        
+        CMP R9, #6
+        ITT EQ
+          MOVEQ R9, #2
+          BEQ num2
+          
+        CMP R9, #7
+        ITT EQ
+          MOVEQ R9, #1
+          BEQ num1
+        
+        CMP R6, #8
+        ITTTT EQ
+          BICEQ R7, R7, #0x03
+          BICEQ R8, R8, #0x10
+          ORREQ R8, R8, #0x01
+          BEQ loop
+        
+        CMP R6, #9
+        ITT EQ
+          MOVEQ R9, #1
+          BEQ num1
+        
+        CMP R6, #10
+        ITT EQ
+          MOVEQ R9, #2
+          BEQ num2
+        
+        CMP R6, #11
+        ITT EQ
+          MOVEQ R9, #3
+          BEQ num3
+        
+        CMP R6, #12
+        ITT EQ
+          MOVEQ R9, #4
+          BEQ num4
+        
+        CMP R6, #13
+        ITT EQ
+          MOVEQ R9, #1
+          BEQ num1
+        
+        CMP R6, #14
+        ITT EQ
+          MOVEQ R9, #2
+          BEQ num2
+        
+        CMP R6, #15
+        ITT EQ
+          MOVEQ R9, #3
+          BEQ num3
+   
 config_N        
         MOV R2, #PORTN_BIT
 	LDR R0, =SYSCTL_RCGCGPIO_R
@@ -55,7 +145,7 @@ waitn	LDR R2, [R0] ; leitura do estado atual
 	TEQ R1, R2 ; clock do port N habilitado?
 	BNE waitn ; caso negativo, aguarda
 
-        MOV R2, #00000011b ; bit 0
+        MOV R2, #00000011b ; bit 0 e bit 1
         
 	LDR R0, =GPIO_PORTN_DIR_R
 	LDR R1, [R0] ; leitura do estado anterior
@@ -73,15 +163,15 @@ config_F
         MOV R3, #PORTF_BIT
 	LDR R0, =SYSCTL_RCGCGPIO_R
 	LDR R1, [R0] ; leitura do estado anterior
-	ORR R1, R2 ; habilita port N
+	ORR R1, R3 ; habilita port F
 	STR R1, [R0] ; escrita do novo estado
         
         LDR R0, =SYSCTL_PRGPIO_R
 waitf	LDR R3, [R0] ; leitura do estado atual
-	TEQ R1, R3 ; clock do port N habilitado?
+	TEQ R1, R3 ; clock do port F habilitado?
 	BNE waitf ; caso negativo, aguarda
 
-        MOV R3, #00010001b ; bit 0
+        MOV R3, #00010001b ; bit 0 e bit 4
         
 	LDR R0, =GPIO_PORTF_DIR_R
 	LDR R1, [R0] ; leitura do estado anterior
